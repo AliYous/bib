5214d27938a08aca9a7f71c78b050bfdf45aca59
Ali <aliyoussef34@hotmail.fr>
2020-01-28 18:45:24 +0100

    Functionnal : fetching of all restaurants data from michelin website and presenting data in a Json file

--- server/michelin.js
+++ server/michelin.js
@@ -2,16 +2,13 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 /**
- * Parse webpage restaurant
- * @param  {String} data - html response
- * @return {Object} restaurant
+ * Fills the restaurants Json with all the restaurants data
+ * @param  {Array} restaurantsUrls - 
+ * @param  {Array} restaurants - //empty at first
  */
-const parse = data => {
-  const $ = cheerio.load(data);
-  const name = $('.section-main h2.restaurant-details__heading--title').text();
-  const experience = $('#experience-section > ul > li:nth-child(2)').text();
+const parse = async(restaurantUrls, restaurants) => {
 
-  return {name, experience};
+
 };
 
 
@@ -23,15 +20,38 @@ const parse = data => {
  * @param  {String}  url
  * @return {Object} restaurant
  */
-module.exports.scrapeRestaurant = async url => {
+module.exports.scrapeRestaurant = async (url, restaurantsArray) => {
   const response = await axios(url);
   const {data, status} = response; 
 
   if (status >= 200 && status < 300) {
-    return parse(data);
-  }
+    const $ = cheerio.load(data)
+    const name = $('.section-main h2.restaurant-details__heading--title').text();
+    const address = $('body > main > div.restaurant-details > div.container > div > div.col-xl-8.col-lg-7 > section.section.section-main.restaurant-details__main > div.restaurant-details__heading.d-none.d-lg-block > ul > li:nth-child(1)').text()
+    const priceAndType = $('body > main > div.restaurant-details > div.container > div > div.col-xl-4.order-xl-8.col-lg-5.order-lg-7.restaurant-details__aside > div.restaurant-details__heading.d-lg-none > ul > li.restaurant-details__heading-price').text().trim().replace(/\s/g, '').split("•");
+    const price = priceAndType[0];
+    const type = "Cuisine : " + priceAndType[1];
+    const experience_array = $('#experience-section > ul > li:nth-child(2)').text().trim().split(' ');
+    const experience = experience_array[experience_array.length - 2] + ' ' + experience_array[experience_array.length - 1];
+    const distinction = 'Bib Gourmand';
+    let phone = 'Non renseigné';
+    if($('body > main > div.restaurant-details > div.container > div > div.col-xl-8.col-lg-7 > section:nth-child(4) > div.row > div:nth-child(1) > div > div:nth-child(1) > div > div > a').attr("href")) {
+     phone = '+' + $('body > main > div.restaurant-details > div.container > div > div.col-xl-8.col-lg-7 > section:nth-child(4) > div.row > div:nth-child(1) > div > div:nth-child(1) > div > div > a').attr("href").replace(/[^0-9]/g, ''); //we use regex to remove the non digits char
+    };
 
-  console.error(status);
+
+    const restaurant = {
+      name: name,
+      address: address,
+      phone: phone,
+      price: price,
+      type: type,
+      experience: experience,
+      distinction: distinction
+    };
+    restaurantsArray.push(restaurant);
+  }
+  else console.error(status);
 
   return null;
 };
@@ -39,8 +59,7 @@ module.exports.scrapeRestaurant = async url => {
 
 module.exports.getNbPages = async (url) => {
   const response = await axios(url);
-  const data = response.data
-  const status = response.status
+  const {data, status} = response; 
   const $ = cheerio.load(data);
 
   const totalRestaurants = $("body > main > section.section-main.search-results.search-listing-result > div > div > div.search-results__count > div.d-flex.align-items-end.search-results__status > div.flex-fill > h1")
@@ -64,8 +83,7 @@ module.exports.fetchRestaurantsUrls = async (nbPages) => {
   let links = [];
   for (let i = 1; i <= nbPages; i++) {
     const response = await axios(`${url}${i}`);
-    const data= response.data;
-    const status = response.status;
+    const {data, status} = response; 
 
     if (status >= 200 && status < 300) {
       const $ = cheerio.load(data);
--- server/sandbox.js
+++ server/sandbox.js
@@ -1,13 +1,24 @@
 /* eslint-disable no-console, no-process-exit */
 const michelin = require('./michelin');
 const axios = require('axios');
+const fs = require('fs');
 
 async function sandbox () {
   try {
     const nbPages = await michelin.getNbPages("https://guide.michelin.com/fr/fr/restaurants/bib-gourmand")
-    console.log(nbPages)
-    const restaurantsUrls = await michelin.fetchRestaurantsUrls(nbPages); //Need to get nb of pages through a method
-    console.log(restaurantsUrls)
+    const restaurantsUrls = await michelin.fetchRestaurantsUrls(1); 
+    let restaurants = [];
+
+    for (url of restaurantsUrls){
+      console.log("scraping " + url);
+      await michelin.scrapeRestaurant(url, restaurants); 
+    }
+
+    console.log(restaurants);
+
+    const json = await JSON.stringify(restaurants, null, 2);
+    fs.writeFileSync('./michelinBib.json', json);
+
     console.log('done');
     process.exit(0);
 
