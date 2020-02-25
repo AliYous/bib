b95e58f421f50d8c16b8ff73617cc645a86cbf0e
ali <aliyoussef34@hotmail.fr>
2020-02-25 18:07:24 +0100

    trying to get the whole fetching of both websites to work

--- server/maitres.js
+++ server/maitres.js
@@ -1,37 +1,117 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
 
 /**
- * Fetch data of all the restaurants on the website (name). We only need the name so we can compare it with our other list
- * @param  {Array} restaurantsArray - array containing data of each restaurant
+ * Get all France located Bib Gourmand restaurants
+ * @param {Array} restaurants - array of all the restaurants
+ */
+module.exports.get = async () => {
+  let restaurants = [];
+
+  const response = await axios({
+    method: 'post',
+    url: 'https://www.maitresrestaurateurs.fr/annuaire/ajax/loadresult#',
+    headers: { 'content-type': 'application/x-www-form-urlencoded' },
+    data: 'page=1&sort=undefined&request_id=ec830a0fb20e71279f65cd4fad4cb137&annuaire_mode=standard'
+  });
+  const { data, status } = response;
+
+  if (status >= 200 && status < 300) {
+    const $ = cheerio.load(data);
+    const totalRestaurants = $("#topbar_nb_persons")
+      .text()
+      .trim()
+      .split(" ");
+    const nbPages = Math.ceil(Number(totalRestaurants[0]) / 10);
+    let links = [];
+    await getAllUrls(links, nbPages);
+    let size = 100;
+    let arrayOfArrays = [];
+    for (let i = 0; i < links.length; i += size) {
+      arrayOfArrays.push(links.slice(i, i + size));
+    }
+    for (array of arrayOfArrays) {
+      await Promise.all(array.map(link => parse(link, restaurants)));
+      await sleep(2000);
+    }
+
+    return restaurants;
+  }
+  else {
+    console.error(status);
+    return restaurants;
+  }
+};
+
+/**
+ * Get the url of all the restaurants on the website
+ * @param  {Array} links - array of link for each restaurant
  * @param  {Number} nbPages - number of pages of results
  */
-module.exports.fetchAllrestaurants = async (restaurantsArray, nbPages) => {
+const getAllUrls = async (links, nbPages) => {
+  console.log("Getting all restaurants urls...")
+  let postRequests = [];
   for (let i = 1; i <= nbPages; i++) {
-    const response = await axios({
+    postRequests.push({
       method: 'post',
       url: 'https://www.maitresrestaurateurs.fr/annuaire/ajax/loadresult#',
       headers: { 'content-type': 'application/x-www-form-urlencoded' },
       data: `page=${i}&sort=undefined&request_id=ec830a0fb20e71279f65cd4fad4cb137&annuaire_mode=standard`
-		});
-    const { data, status } = response;
-
-    if (status >= 200 && status < 300) {
-			const $ = cheerio.load(data);
-			
-      $('.single_desc').each((index, value) => {
-				let url = `https://www.maitresrestaurateurs.fr${$(value).find('.single_libel a').attr('href')}`;
-				let name = $(value).find('.single_libel a').text().replace(/\s*\(.*?\)\s*/g, '');
-
-				let restaurant = {
-					name: name,
-					url: url
-				};
-
-				restaurantsArray.push(restaurant);				
-			});
-    }
-    else console.error(status);
+    })
   }
-};
+  let size = 50;
+  let arrayOfArrays = [];
+  for (let i = 0; i < postRequests.length; i += size) {
+    arrayOfArrays.push(postRequests.slice(i, i + size));
+  }
+  for (array of arrayOfArrays) {
+    await Promise.all(array.map(async request => {
+      const response = await axios(request);
+      const { data, status } = response;
+
+      if (status >= 200 && status < 300) {
+        const $ = cheerio.load(data);
+        $('.single_libel a').each((index, value) => {
+          let link = $(value).attr('href');
+          links.push(`https://www.maitresrestaurateurs.fr${link}`);
+        });
+      }
+      else console.error(status);
+    }));
+    await sleep(2000);
+  }
+}
+
+/**
+ * Parse webpage restaurant
+ * @param  {Array} links - array of link for each restaurant
+ * @param  {Array} restaurants - array of object restaurant
+ */
+const parse = async (link, restaurants) => {
+  console.log(`🕵️‍♀️  browsing ${link}`);
+  const response = await axios(link);
+  const { data, status } = response;
+
+  if (status >= 200 && status < 300) {
+    const $ = cheerio.load(data);
+    const nom = $('#module_ep > div.ep-container.container > div > div > div.ep-content-left.col-md-8 > div > div.ep-section.ep-section-parcours.row > div > div > div.section-item-right.text.flex-5 > span:nth-child(1) > strong').text();
+    const adresse = $('#adresse_pro_1_map').text().trim().replace(/\s+\n+/g, '') + ' France';
+    let telephone;
+    if ($('#module_ep > div.ep-container.container > div > div > div.ep-content-left.col-md-8 > div > div.ep-section.ep-section-parcours.row > div > div > div.section-item-right.text.flex-5').text().replace(/\s/, '').match(/(?:\+33\s|0)[1-9](?:\s\d{2}){4}/g) !== null)
+      telephone = $('#module_ep > div.ep-container.container > div > div > div.ep-content-left.col-md-8 > div > div.ep-section.ep-section-parcours.row > div > div > div.section-item-right.text.flex-5').text().replace(/\s/, '').match(/(?:\+33\s|0)[1-9](?:\s\d{2}){4}/g)[0];
+    else telephone = 'Non renseigné';
+    const restaurant = {
+      nom: nom,
+      adresse: adresse,
+      telephone: telephone
+    };
+    restaurants.push(restaurant);
+  }
+  else console.error(status);
+}
--- server/sandbox.js
+++ server/sandbox.js
@@ -14,13 +14,22 @@ const fs = require('fs');
 async function sandbox() {
   try {
 
-    let links = [];
-    await maitres.fetchAllrestaurants(links, 2)
-    console.log(links) 
+  
+    console.log('🕵️‍♀️  browsing https://www.maitresrestaurateurs.fr');
+    const restaurants2 = await maitres.get();
+    const json2 = await JSON.stringify(restaurants2, null, 2);
+    fs.writeFileSync('server/MaitreRestaurateur.json', json2);
 
 
-
-
+    
+    console.log('🕵️‍♀️  browsing https://guide.michelin.com/fr');
+    const nbPages = michelin.getNbPages('https://guide.michelin.com/fr/fr/restaurants/bib-gourmand/');
+    const links = michelin.fetchRestaurantsUrls(nbPages);
+    let restaurants = []
+    
+    for (let i = 0; i < links.length; i += size) {
+      arrayOfArrays.push(links.slice(i, i + size));
+    }
     process.exit(0);
 
   } catch (e) {
